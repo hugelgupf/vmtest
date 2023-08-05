87f010905f8c1e5b889e68fe9d776cf6b0b4def5
Chris Koch <chrisko@google.com>
2023-08-05 16:57:02 -0700

    QEMU pkg: test VM lifetime bits
    Also fixes serial console lifetime & expectancy
    
    Signed-off-by: Chris Koch <chrisko@google.com>

---
+++ qemu/qemutest1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	fmt.Println("I AM HERE")
+
+	if err := unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF); err != nil {
+		log.Fatalf("Failed to shut down: %v", err)
+	}
+}
