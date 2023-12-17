e42c29a4f3e992563685720a3d6ddda7325d4422
Chris Koch <chrisko@google.com>
2023-12-17 15:49:34 -0800

    Test that kcov is collected
    Signed-off-by: Chris Koch <chrisko@google.com>

--- tests/gohello/helloworld_test.go
+++ tests/gohello/helloworld_test.go
@@ -1,14 +1,33 @@
 package helloworld
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/hugelgupf/vmtest"
 	"github.com/hugelgupf/vmtest/guest"
+	"github.com/hugelgupf/vmtest/testtmp"
 )
 
 func TestStartVM(t *testing.T) {
-	vmtest.RunGoTestsInVM(t, []string{"github.com/hugelgupf/vmtest/tests/gohello"})
+	vmtest.SkipWithoutQEMU(t)
+
+	kcovDir := os.Getenv("VMTEST_KERNEL_COVERAGE_DIR")
+	if kcovDir == "" {
+		kcovDir = testtmp.TempDir(t)
+		os.Setenv("VMTEST_KERNEL_COVERAGE_DIR", kcovDir)
+	}
+
+	// Kernel coverage is copied to kcovDir during t.Cleanup, so induce it
+	// before the test is over by using a sub-test.
+	t.Run("test", func(t *testing.T) {
+		vmtest.RunGoTestsInVM(t, []string{"github.com/hugelgupf/vmtest/tests/gohello"})
+	})
+
+	if _, err := os.Stat(filepath.Join(kcovDir, "TestStartVM", "test", "0", "kernel_coverage.tar")); err != nil {
+		t.Fatalf("Kernel coverage file not found: %v", err)
+	}
 }
 
 func TestHelloWorld(t *testing.T) {
