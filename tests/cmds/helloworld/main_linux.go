d015c07039d6a4899623371bf8af249fd5c7c252
Chris Koch <chrisko@google.com>
2024-01-15 03:13:07 +0000

    helloworld: add repeat flag
    Signed-off-by: Chris Koch <chrisko@google.com>

--- tests/cmds/helloworld/main_linux.go
+++ tests/cmds/helloworld/main_linux.go
@@ -2,14 +2,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
 	"golang.org/x/sys/unix"
 )
 
+var n = flag.Int("n", 1, "How many times to repeat Hello world")
+
 func main() {
-	fmt.Println("Hello world")
+	flag.Parse()
+
+	for i := 0; i < n; i++ {
+		fmt.Println("Hello world")
+	}
 
 	if err := unix.Reboot(unix.LINUX_REBOOT_CMD_POWER_OFF); err != nil {
 		log.Fatalf("Failed to shut down: %v", err)
