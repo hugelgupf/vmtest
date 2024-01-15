aa8d2e9c4a1ddea5e8f499457446de4a0ae8a19b
Chris Koch <chrisko@google.com>
2024-01-15 02:33:46 +0000

    Remove unused uroot.vmtest arg to guests
    Signed-off-by: Chris Koch <chrisko@google.com>

--- vmtest.go
+++ vmtest.go
@@ -229,9 +229,7 @@ func startVM(t testing.TB, o *VMOptions) *qemu.VM {
 		qemu.LogSerialByLine(qemu.PrintLineWithPrefix(o.ConsoleOutputPrefix, t.Logf)),
 		// Tests use this env var to identify they are running inside a
 		// vmtest using SkipIfNotInVM.
-		//
-		// Older tests use the presence of uroot.vmtest in the kernel command-line.
-		qemu.WithAppendKernel("VMTEST_IN_GUEST=1", "uroot.vmtest"),
+		qemu.WithAppendKernel("VMTEST_IN_GUEST=1"),
 		qemu.VirtioRandom(),
 	}
 	if o.SharedDir != "" {
